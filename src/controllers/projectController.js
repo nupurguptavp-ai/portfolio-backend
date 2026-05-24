783a1020332a3691f0cad1e48b8aae684bb211b2
nupurguptavp-ai <nupurgupta.vp@gmail.com>
2026-05-24 17:45:18 +0530

    Checking pm2 logs

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -20,7 +20,7 @@ const createProject = async (req, res) => {
         )
         res.status(201).json(result.rows[0])
     } catch (error) {
-        console.log(error);
+        console.error("GET PROJECTS ERROR:", error);
         res.status(500).json({
             message: "Internal server error"
         });
@@ -30,15 +30,15 @@ const createProject = async (req, res) => {
 
 const updateProject = async (req, res) => {
     try {
-        const { id }= req.params
-        const { title, description} = req.body;
-        if(!title || !description) {
+        const { id } = req.params
+        const { title, description } = req.body;
+        if (!title || !description) {
             return res.status(400).json({
                 message: "Missing required fields"
             });
         }
-        const result = await pool.query (
-            `UPDATE projects SET title = $1, description = $2 WHERE id = $3 RETURNING *`,[title, description,id]
+        const result = await pool.query(
+            `UPDATE projects SET title = $1, description = $2 WHERE id = $3 RETURNING *`, [title, description, id]
         )
         res.json(result.rows[0]);
     } catch (error) {
@@ -51,14 +51,14 @@ const updateProject = async (req, res) => {
 
 const deleteProject = async (req, res) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const result = await pool.query(
-            `DELETE FROM projects WHERE id = $1`,[id]
+            `DELETE FROM projects WHERE id = $1`, [id]
         )
-        res.json({message: "Project Deleted"})
-    } catch(error) {
+        res.json({ message: "Project Deleted" })
+    } catch (error) {
         console.log(error);
-        res.status(500).json({message: "Internal server error"})
+        res.status(500).json({ message: "Internal server error" })
     }
 }
 module.exports = { getProjects, createProject, updateProject, deleteProject };
